86ecaffbb3b99b84f9571034155cfaba34af8dc8
The-click <0leg.19-94@mail.ru>
2021-09-12 16:03:35 +0300

    add adaptive

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // Slide details practice
 
 const detailsPractice = document.querySelectorAll('.practice__details');
+const burgerMenu = document.querySelector('.header__burger');
+const headerMenuBlock = document.querySelector('.header__subabout');
 
 function tooglePractice(item) {
     console.log(item);
@@ -11,4 +13,13 @@ function tooglePractice(item) {
         }
     })
 }
+
+function toggleBurger() {
+    burgerMenu.classList.toggle('active');
+    headerMenuBlock.classList.toggle('active');
+
+}
+
+
 detailsPractice.forEach(i => i.addEventListener('toggle', (e) => tooglePractice(i)))
+burgerMenu.addEventListener('click', toggleBurger)
